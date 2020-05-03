84049ac80ff292168c242b1648232ad078e28b07
Daniel Lemire <lemire@gmail.com>
2020-05-03 16:56:30 -0400

    Using zeros.

--- scripts/sse_utf16_to_utf8_simple.py
+++ scripts/sse_utf16_to_utf8_simple.py
@@ -9,22 +9,18 @@ def print_simple():
   ## assumes little endian layout
   t = []
   s = []
-  for i in [0x10]:##range(1<<8):
-    m = [255 for i in range(16)]
+  for i in range(1<<8):
+    m = [0 for i in range(16)]
     pos = 0
     for j in range(8):
         # we detect non-ascii, so ascii => 0 bits
         if((i & (1<<j)) != (1<<j)): # ascii
             m[pos] = j * 2 # ascii is always at even locations 0,1,...
-            m[pos] = 0
             pos += 1
         else:
             # need to copy two bytes
             m[pos] = (j * 2 + 1) | 0b11100000 # first byte is more significant
-            m[pos] = 0b11100000 
             m[pos + 1] = j * 2 | 0b11000000
-            m[pos + 1] = 0b11100000 
-
             pos += 2
     s.append(pos)
     t.append(m)
